d27fa8ec94560edbb7c0562e3bd621c16f8a2e82
Martin Cerna <martin.cernaa@gmail.com>
2024-09-24 10:19:19 -0500

    Update script.js

--- app.js
+++
@@ -1,8 +0,0 @@
-let character = 'Hello';
-let count = 8;
-let rows = ["Naomi", "Quincy", "CamperChan"];
-console.log(rows);
-rows.push("freeCodeCamp");
-console.log(rows);
-let popped = rows.pop();
-console.log(popped);
--- pyramid-generator/script.js
+++ pyramid-generator/script.js
@@ -3,11 +3,12 @@ const count = 8;
 const rows = [];
 
 function padRow(rowNumber, rowCount) {
-   return character.repeat(rowNumber);
+   return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
 
-for (let i = 0; i < count; i = i + 1) {
-   rows.push(character.repeat(i + 1))
+for (let i = 1; i < count; i++) {
+
+   rows.push(padRow(i, count))
 }
 
 let result = ""
