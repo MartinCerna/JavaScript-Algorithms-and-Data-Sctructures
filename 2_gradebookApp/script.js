c7d76e46f0ab34af1fe6b268af6bc854c80ea698
Martin Cerna <martin.cernaa@gmail.com>
2024-10-03 06:33:24 -0500

    Update script.js

--- 2_gradebookApp/script.js
+++ 2_gradebookApp/script.js
@@ -2,10 +2,19 @@ function getAverage(scores) {
    let sum = 0;
 
    for (const score of scores) {
-      sum += score;      
+      sum += score;
    }
-
    return sum / scores.length;
+}
+
+function getGrade(score) {
+   if (score === 100) {
+      return "A++"
+   } else if (90 <= score >= 99) {
+      return "A"
+   } else if (80 <= score >= 89) {
+      
+   }
 }
 
 console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
